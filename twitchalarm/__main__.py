f3198cbdb099257c35aeae735d4e5b8a54b1381f
MarkKoz <KozlovMark@gmail.com>
2019-05-07 00:46:48 -0700

    Replace config file with argparse
    * Run program as a module instead

---
+++ twitchalarm/__main__.py
@@ -0,0 +1,36 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import os
+
+from twitchalarm.alarm import checkLive
+
+parser = ArgumentParser(
+    prog="twitch-alarm",
+    description="Sound an alarm when a Twitch channel goes live."
+)
+parser.add_argument(
+    "channels",
+    nargs="+",
+    help="Channels to check if live."
+)
+parser.add_argument(
+    "--client_id",
+    required=False if "TWITCH_CLIENT_ID" in os.environ else True,
+    default=os.environ.get("TWITCH_CLIENT_ID"),
+    help="Twitch API client ID. Required unless specified with the environment"
+         "variable TWITCH_CLIENT_ID."
+)
+parser.add_argument(
+    "--frequency", "-f",
+    type=int,
+    default=60,
+    help="Frequency, in seconds, at which to check if a channel is live."
+)
+parser.add_argument(
+    "--sound", "-s",
+    type=lambda p: Path(p).resolve(True),
+    help="Path to the sound file to use for the alarm. Supports only wav files."
+)
+args = parser.parse_args()
+
+checkLive(args)
--- twitchalarm/alarm.py
+++ twitchalarm/alarm.py
@@ -1,32 +1,28 @@
-from typing import Dict
 import datetime
-import json
 import time
 
 from dateutil import parser
 import requests
 
-from audio import WavePlayer
-
-config = dict()
+from twitchalarm.audio import WavePlayer
 
 urlBase = "https://api.twitch.tv/helix/"
 
-def getStreams(channels):
+def getStreams(channels, client_id):
     response = requests.get(f"{urlBase}streams",
                             params = {
                                 "type": "live",
                                 "user_login": [c for c in channels]
                             },
-                            headers = {"Client-ID": config["clientID"]})
+                            headers = {"Client-ID": client_id})
     response.raise_for_status()
 
     return response.json()
 
-def getDisplayName(userID):
+def getDisplayName(userID, client_id):
     response = requests.get(f"{urlBase}users",
                             params = {"id": userID},
-                            headers = {"Client-ID": config["clientID"]})
+                            headers = {"Client-ID": client_id})
     response.raise_for_status()
     data = response.json()["data"]
 
@@ -36,24 +32,20 @@ def getDisplayName(userID):
 
     return data[0]["display_name"]
 
-def loadConfig() -> Dict:
-    with open("Configuration.json") as file:
-        return json.load(file)
-
-def checkLive(channels):
+def checkLive(config):
     live = False
 
     while not live:
-        time.sleep(config["frequency"])
+        time.sleep(config.frequency)
         timestamp = datetime.datetime.now().replace(microsecond = 0).isoformat()
         print(f"[{timestamp}] Checking if live...")
 
         try:
-            streams = getStreams(channels)
+            streams = getStreams(config.channels, config.client_id)
             data = streams["data"]
 
             if data:
-                onLive(data)
+                onLive(data, config.client_id, config.sound)
                 live = True
                 break
         except (requests.exceptions.HTTPError,
@@ -61,26 +53,19 @@ def checkLive(channels):
             print(e.args[0])
             continue
 
-def onLive(streams):
+def onLive(streams, client_id, sound):
     for s in streams:
         timestamp = datetime.datetime.now().replace(microsecond = 0).isoformat()
-        name = getDisplayName(s["user_id"])
+        name = getDisplayName(s["user_id"], client_id)
         started = parser.parse(s["started_at"]).\
             astimezone(tz = None).replace(tzinfo = None).isoformat()
         print(f"[{timestamp}] {name} went live at {started}")
 
-    playAlarm()
+    playAlarm(sound)
 
-def playAlarm():
-    sound = WavePlayer(config["sound"])
+def playAlarm(sound_path):
+    sound = WavePlayer(sound_path)
     sound.play()
 
     input("Enter anything to stop the alarm.")
     sound.stop()
-
-def main():
-    checkLive(config["channels"])
-
-if __name__ == "__main__":
-    config = loadConfig()
-    main()
